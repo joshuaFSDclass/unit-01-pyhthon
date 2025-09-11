f85cac314c4b356f6de4e1ce19f03b28eace0adf
joshuaFSDclass <jlewinbrooklynsteamcenter.org>
2025-09-11 10:53:09 -0400

    i did the first two i forgot all the python we did last year

---
+++ 01_data_types.py
@@ -0,0 +1,55 @@
+"""
+TASK 1:
+Create a float variable, and then convert it to an integer
+Print both the original variable and the converted integer.
+
+"""
+number_float = 30.89
+print(number_float)
+number_int = int(number_float)
+print(number_int)
+# I turned 30.0 into the integer 30
+"""
+TASK 2:
+Write code that takes a number as input and prints whether 
+it's positive, negative, or zero using if-elif-else statements.
+"""
+positive_Or_negative = int(input("number"))
+if positive_Or_negative > 0 :print("this number is positive")
+# takes a number input checks if it's greater than zero, then prinst that it is positive 
+elif positive_Or_negative < 0 :print("this number is negative")
+# takes a number input checks if it's ldss than zero, then prints that it is negative
+elif positive_Or_negative == 0 : print("this number is zreo")
+ # determines that the input is 0 then prints that it is Zero
+
+"""
+TASK 3:
+
+Write code that takes two numbers as input (an integer and a float), 
+performs addition, subtraction, multiplication, and division, and prints the results.
+"""
+
+
+"""
+TASK 4:
+
+Create a dictionary with keys as fruit names and values as their respective quantities. 
+Then print out the quantity of one of the fruits.
+"""
+
+
+"""
+TASK 5:
+
+Create a string variable with that is a list of numbers and convert that string into a tuple.
+Then print out the both the original string and tuple.
+"""
+
+"""
+TASK 6:
+
+Create a list of your favorite subjects (as strings). 
+Use the join() function to combine all subjects into a single string separated by commas.
+Then create another version using " - " as the separator.
+Print both the original list and both joined strings.
+"""
