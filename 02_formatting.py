17591a2edf0796b7976128d8c2282adaee92617f
joshua <jlewin@brooklynsteamcenter.org>
2025-09-14 23:00:09 -0400

    almot finnished

--- 02_formatting.py
+++ 02_formatting.py
@@ -19,10 +19,14 @@ Write code that checks if a user inputs an empty string
 If the string is empty, print "invalid" otherwise print "valid"
 """
 User_intput = input("input any string of your choice: " )
+#User input a string 
 if User_intput.strip() == "":
+    #checks whether or not the user input any other string input other than a space
     print("invalid")
+    #if conditions are met it prints "invalid"
 else:
     New_user_input = User_intput.strip()
+    #if conditions are not met print "valid"
     print("valid")
     print(User_intput)
 
@@ -32,14 +36,21 @@ TASK 3:
 Write a program that will replace the word "cat" with the word "dog"
 It should replace all occurances regardless of captilization 
 """
-
+animal = "The little ginger cat crouched low, stalking a sunbeam across the floor. This clever cat knew exactly how to find the warmest spot for an afternoon nap. Soon, the exhausted cat was dreaming about catching the elusive red dot, a truly content cat."
+# A string with the word dog in it
+new_animal = animal.lower().replace("cat","dog")
+# replace function that replaces every uccurance of the word cat and changing it to the word dog
+print(new_animal)
 
 """
 TASK 4:
 
 Write a program that takes a person's name and age as input and prints it
 """
-
+name = input("what is your name: ")
+age = input("what is your age: ")
+scentence = f"your name is {name} and your age is {age}"
+print(scentence)
 
 """
 TASK 5:
